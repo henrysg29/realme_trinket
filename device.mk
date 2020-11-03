8f10363b7951edefe914b69168e85608ef42041c
henrysg <39873175+henrysg@users.noreply.github.com>
2020-11-03 09:49:17 +0700

    realme_trinket : hdpi > xhdpi

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -151,6 +151,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
+# WFD
 PRODUCT_BOOT_JARS += \
     WfdCommon
 

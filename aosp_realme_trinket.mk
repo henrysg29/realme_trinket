268273645f8b37513806dcca6708ad4cf8abb795
henrysg <39873175+henrysg@users.noreply.github.com>
2020-11-03 09:48:14 +0700

    realme_trinket : bringup AOSP
    also change bootanimation resolution

--- aosp_realme_trinket.mk
+++ aosp_realme_trinket.mk
@@ -7,11 +7,11 @@
 $(call inherit-product, device/realme/realme_trinket/device.mk)
 
 # Inherit some common Aosip stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Setup Gapps options
 IS_PHONE := true
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_MINIMAL_APPS := false
@@ -21,7 +21,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := false
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := realme_trinket
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := derp_realme_trinket
+PRODUCT_NAME := aosp_realme_trinket
 
 BUILD_FINGERPRINT := "trinket-user-10-QKQ1.200209.002--release-keys"
 
